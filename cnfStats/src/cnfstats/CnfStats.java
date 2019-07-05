3541fab051151be7c33341f0a1c2ff883aea5193
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-07-05 12:28:39 +0200

    CnfStats frissítése
    Elvégzett feladatok: Segédprogram nélkül is a megfelelő felépítésű kimenet áll mostmár elő.

--- cnfStats/src/cnfstats/CnfStats.java
+++ cnfStats/src/cnfstats/CnfStats.java
@@ -41,16 +41,22 @@ public class CnfStats {
             printHelp();
             return;
         }
+        String folderPath = "D:\\Projektmunkák\\EKE-PROBLEM-CLASSIFIER-WITH-NEURAL-NETWORK\\cnfStats\\files\\AIS\\ki";
         
-        List<String> files = getFilenames("D:\\Projektmunkák\\EKE-PROBLEM-CLASSIFIER-WITH-NEURAL-NETWORK\\cnfStats\\files\\DIMACS\\SSA\\kicsomagolva_8");
-        for (int i = 0; i < files.size(); i++) {
-            DIMACSStatisticsBuilder sb = new DIMACSStatisticsBuilder(new DIMACSReader(files.get(i)));
-            sb.finalizy();
-            sb.print2("SSA", "SSA_8.txt");
-            if(i % 10 == 0) System.out.println(i + "/" + files.size() );
+        if(getFilenames(folderPath) != null){
+            List<String> files = getFilenames(folderPath);
+            for (int i = 0; i < files.size(); i++) {
+                DIMACSStatisticsBuilder sb = new DIMACSStatisticsBuilder(new DIMACSReader(folderPath + "\\" + files.get(i)));
+                sb.finalizy();
+                sb.printToFile("SSA", "SSA_8.txt");
+                if(i % 10 == 0) System.out.println(i + "/" + files.size() );
+            }
+            System.out.println(files.size() + "/" + files.size() );
+            System.out.println("Kész!");
         }
-        System.out.println(files.size() + "/" + files.size() );
-        System.out.println("Kész!");
+        else{
+            System.out.println("A fileok nem találhatóak!");
+        }
     }
     
     private static void printHelp() {
@@ -63,13 +69,14 @@ public class CnfStats {
     public static List<String> getFilenames(String folderPath){
         List<String> filenames = new ArrayList<>();
         File[] listOfFiles = new File(folderPath).listFiles();
-
-        for (File file : listOfFiles){
-            if (file.isFile()){
-                filenames.add(file.getName());
+        if(listOfFiles != null){
+            for (File file : listOfFiles){
+                if (file.isFile()){
+                    filenames.add(file.getName());
+                }
             }
+            return filenames;
         }
-        
-        return filenames;
+        else return null;
     }
 }
