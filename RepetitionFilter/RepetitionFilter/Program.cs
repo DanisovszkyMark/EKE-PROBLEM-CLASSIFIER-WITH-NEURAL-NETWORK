392598717a3a9bbebc72a565b6331491e4533dd0
DanisovszkyMark <32265752+DanisovszkyMark@users.noreply.github.com>
2019-07-14 12:06:14 +0200

    Ismétlődés szűrő megírása
    Elvégzett feladatok: Egy olyan alkalmazást készítettem, mely egy file-ból kiszűri az ismétlődéseket. A felhasználása eléggé fontos, péládul fényt derített arra, hogy az SW-GCP osztályhoz tartozó előre megadott 901 feladat mindössze egyetlen egy feladat sokszorozása.

---
+++ RepetitionFilter/RepetitionFilter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepetitionFilter
+{
+    class Program
+    {
+        private static List<string> lines = new List<string>();
+        private static string fromPath = "SSA_8";
+        private static string toPath = "SSA";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A folyamat megkezdéséhez nyomj [ENTER]-t");
+            Console.ReadLine();
+
+            StreamReader sr = new StreamReader(fromPath + ".txt");
+            string actualLine = "";
+
+            Console.WriteLine("Kimenet előállítása...");
+            while (!sr.EndOfStream)
+            {
+                actualLine = sr.ReadLine();
+                if (!lines.Contains(actualLine)) lines.Add(actualLine);
+            }
+            sr.Close();
+
+            Console.WriteLine("Fájl elkészítése...");
+            StreamWriter sw = new StreamWriter(toPath + "_" + lines.Count + ".txt");
+            for (int i = 0; i < lines.Count; i++)
+            {
+                sw.WriteLine(lines[i]);
+            }
+            sw.Close();
+
+            Console.WriteLine("Folyamat kész!");
+            Console.ReadLine();
+        }
+    }
+}
